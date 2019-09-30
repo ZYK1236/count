c274ba6a5a704a3bbef8f4280a17bb9368a24c9b
ZYK1236 <45277494+ZYK1236@users.noreply.github.com>
2019-09-30 20:12:20 +0800

    Update Cou.js

--- Cou.js
+++ Cou.js
@@ -8,12 +8,20 @@ function fresh()
 function clea()
 {
     sum = 0;
+    flag = 0;
     document.getElementById("text").innerHTML = '0';
 }
 
 function de()
 {
     var num_len = document.getElementById("text").innerHTML.length;
+    if(num_len == 1)
+    {
+        flag = 0;
+        sum = 0;
+        document.getElementById("text").innerHTML = '0';
+        return;
+    }
     var str = document.getElementById("text").innerHTML;
     str = str.substr(0,num_len-1);
     document.getElementById("text").innerHTML = str;
@@ -30,86 +38,25 @@ function display_num(n)
         num2 = num2 + num1;
         document.getElementById("text").innerHTML = num2;        
     }
+    flag = 1;
 }
 
 function display_tool(n)
 {
     if(n!='=')
     {
-        switch(n)
-        {
-            case "+":flag = 1;
-            break;
-            case "-":flag = 2;
-            break;
-            case "/":flag = 3;
-            break;
-            case "*":flag = 4;
-            break;
-            // case "Math.PI":flag = 5;
-            // break;
-            // case "Math.E" :flag = 6;
-            // break;
-        }    // flag代表加减乘除等符号的代号
         var str1 = document.getElementById("text").innerHTML;
         var str2 = document.getElementById(n).innerHTML;
-        str1 = str1+str2;
-        document.getElementById("text").innerHTML = str1;
+        if(str2 == '-' && flag == 0)
+            document.getElementById("text").innerHTML = str2;
+        else{
+            str1 = str1+str2;
+            document.getElementById("text").innerHTML = str1;
+        }
     }
     else
     {
         var str1 = document.getElementById("text").innerHTML;
-        /*switch(flag)
-        {
-            case 1: var my_num = str1.split("+");
-                    num_len = my_num.length;
-                    for (var i = 0;i < num_len;i++){
-                        if(my_num[i] == 'π')
-                            sum = sum + Math.PI;
-                        else
-                            sum = sum + parseFloat(my_num[i]);
-                    }
-                    break;
-            case 2: var my_num = str1.split("-");
-                    num_len = my_num.length;
-                    if(my_num[0] == 'π')
-                        sum = Math.PI;
-                    else
-                        sum = parseFloat(my_num[0]);
-                    for (var i = 1;i < num_len;i++)
-                    {
-                        if(my_num[i] == 'π')
-                            sum = sum - Math.PI;
-                        else
-                            sum = sum - parseFloat(my_num[i]);
-                    }
-                     break;
-            case 3: var my_num = str1.split("/");
-                    num_len = my_num.length;
-                    if(my_num[0] == 'π')
-                        sum = Math.PI;
-                    else
-                        sum = parseFloat(my_num[0]);
-                    for (var i = 1;i < num_len;i++)
-                    {
-                        if(my_num[i] == 'π')
-                            sum = sum / Math.PI;
-                        else
-                            sum = sum / parseFloat(my_num[i]);
-                    }
-                    break;   
-            case 4: var my_num = str1.split("*");
-                    num_len = my_num.length;
-                    sum = 1;
-                    for (var i = 0;i < num_len;i++)
-                    {
-                        if(my_num[i] == 'π')
-                            sum = sum * Math.PI;
-                        else
-                            sum = sum * parseFloat(my_num[i]);
-                    }
-                    break;                              
-        }*/
         var num_len = str1.length;
         str1 = str1.replace(/π/g, 'Math.PI');
         str1 = str1.replace(/e/g,'Math.E');
